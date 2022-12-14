21ce28874861d0568b797596b3f716addbaacbbb
LizCuba <114257401+LizCuba@users.noreply.github.com>
2022-12-13 21:02:04 -0500

    Add files via upload

---
+++ ConfigServerLizFinal/src/test/java/com/example/configserverlizfinal/ConfigServerLizFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configserverlizfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerLizFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
